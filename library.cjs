aed847080b7e6df2131801e8e3c2d765e162725b
Jayashree6322 <jayashree06032002@gmail.com>
2025-08-20 12:07:08 +0530

    Updating the users borrowing details.

--- library.cjs
+++ library.cjs
@@ -10,26 +10,39 @@ const {
 
 const availableBooks = () => books.filter(book => book.available);
 
-const checkBookLimit = users.map((user) => 
-  user.borrowedBooks.length >= maxBorrowLimit ? "Limit reached" : "Can borrow more books");
+const checkBookLimit = (id) => {
+  const user = users.find((user) => user.id === id);
+    return user.borrowedBooks.length < maxBorrowLimit ? "Can borrow more books" : "Limit reached";
+};
 
 const dueDate = borrowDurationDays * 24 * 60 * 60 * 1000;
 
-const setDueDate = () => {
-  const currentDate = new Date().toLocaleDateString();
-  const returnDate = new Date(Date.now() + dueDate).toLocaleDateString();
-return { 
-  CurrentDate: currentDate,
-  ReturnDate: returnDate 
-}};
+const setDueDate = () => new Date(Date.now() + dueDate).toLocaleDateString();
 
 const checkBookAvailability = (id) => {
-  const book = books.find(book => book.id === id);
+  const book = books.find(book => book.bookId === id);
   return book.available ? "Available" : "Not Available" ;
 };
 
+const updatedUser = users.map((user, bookId) => ({
+    ...user,
+    borrowedBooks: [
+      ...user.borrowedBooks,
+      { bookId, 
+        dueDate: setDueDate()
+      }
+    ]
+}));
+
+
+const updateBorrowBook = (id, book) => {
+  book.available = false
+  return users.map((user) => user.id === id ? updatedUser : user);
+};
 
 console.log("List of available books:",availableBooks());
-console.log("To check the book limit:",checkBookLimit);
-console.log("Setting the due date for user:",setDueDate());
+console.log("To check the book limit:",checkBookLimit(2019014));
 console.log("Checking the book availability:",checkBookAvailability(5));
+console.log("Updating the borrow details of the user",JSON.stringify(updateBorrowBook(2019014, 2),null,2));
+
+
